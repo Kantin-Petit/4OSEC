84d37b5c1ab9318d9b6f4e5ea370400302ff1682
Kantin-Petit <kantin.dev@gmail.com>
2024-10-23 16:21:03 +0200

    correction s3

--- bool_logic.py
+++ bool_logic.py
@@ -1,21 +1,27 @@
-a = 1
-b = 1
-c = 0
-d = 0
+class Person():
+    def is_major(self):
+        return self.age >= 18
+
+toto = Person()
+toto.age = 20
+titi = Person()
+titi.age = 32
+nana = Person()
+nana.age = 12
+nono = Person()
+nono.age = 8
+
+a = toto.is_major()
+b = titi.is_major()
+c = nana.is_major()
+d = nono.is_major()
+
 f = not(a and (not b) and (a or b)) or (not a and b) and not(a or (not b) or (not c)) # f = b or not a
 
 g = not((c or d) and a and not(c) and d and (not a) and c or (not d)) # g = d
  
 s1 = a and (a or b) # s1 = a
 s2 = (a or b) and ((not a) or b) # s2 = b
-s3 = (a and (not b) or c) and (a or (not b)) # s3 = a and not(b)
+s3 = (a and (not b) or c) and (a or (not b)) # s3 = c and (a or not b) 
 s4 = (a and b) or c and (not a) or (b and c) and (not b) # s4 = b or c
 s5 = a or b or ((not c) and b) or (a or (not c)) or not((not a) or b or ((not a) and c))  # s5 = a or b
-
-print("f:"+ str(f))
-print("g:"+ str(g))
-print("s1:"+ str(s1))
-print("s2:"+ str(s2))
-print("s3:"+ str(s3))
-print("s4:"+ str(s4))
-print("s5:"+ str(s5))
