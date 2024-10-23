a4ba0bb787df377dc3a8215db593436dbe9e1c1e
Kantin-Petit <kantin.dev@gmail.com>
2024-10-23 16:25:06 +0200

    rename s5 to s6 and add s5

--- bool_logic.py
+++ bool_logic.py
@@ -24,4 +24,5 @@ s1 = a and (a or b) # s1 = a
 s2 = (a or b) and ((not a) or b) # s2 = b
 s3 = (a and (not b) or c) and (a or (not b)) # s3 = c and (a or not b) 
 s4 = (a and b) or c and (not a) or (b and c) and (not b) # s4 = b or c
-s5 = a or b or ((not c) and b) or (a or (not c)) or not((not a) or b or ((not a) and c))  # s5 = a or b
+s5 = (a or b or c) and (not a or b or c) or (a and b) or c and d # s5 = b or c
+s6 = a or b or ((not c) and b) or (a or (not c)) or not((not a) or b or ((not a) and c))  # s5 = a or b
