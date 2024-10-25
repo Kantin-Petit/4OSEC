2f579b77591e731df6357a837e82ec5d6ed81471
Kantin-Petit <kantin.dev@gmail.com>
2024-10-25 09:08:27 +0200

    update hello.c with type of var

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello, world\n");
+int main(int argc, char* const argv[]) {
+	char* addr = &argv[1];
+	printf("Hello p, %p\n", addr);
+	printf("Hello s&, %s\n", &addr);
+	printf("Hello i, %i\n", argv[1]);
+	printf("Hello s, %s\n", argv[1]);
 	return 0;
 };
